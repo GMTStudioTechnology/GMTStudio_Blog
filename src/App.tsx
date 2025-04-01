f1fb4673f71d7acb375a0055fd10336dae77d5e0
GMTStudiotech2024 <153701583+GMTStudiotech2024@users.noreply.github.com>
2025-04-01 13:45:44 +0800

    Trying to fix a bug

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,21 @@ import Blog from "./components/layout/Blog";
 import Navbar from "./components/layout/Navigation_bar";
 import Blog_1 from "./components/layout/UI/Blog/Blog1";
 import Blog_2 from "./components/layout/UI/Blog/Blog2"
+
 function App() {
   return (
     <div className="App">
-    <Navbar/>
-    <Router>
-      <div className="min-h-screen flex flex-col">
-        <Routes>
-          <Route path="/" element={<MainPage/>} />
-          <Route path="/blog" element={<Blog/>}/>
-          <Route path="/blog1" element={<Blog_1/>}/>
-          <Route path="/blog2" element={<Blog_2/>}/>
-        </Routes>
-      </div>
-
-    </Router>
-
+      <Router>
+        <Navbar/>
+        <div className="min-h-screen flex flex-col">
+          <Routes>
+            <Route path="/" element={<MainPage/>} />
+            <Route path="/blog" element={<Blog/>}/>
+            <Route path="/blog1" element={<Blog_1/>}/>
+            <Route path="/blog2" element={<Blog_2/>}/>
+          </Routes>
+        </div>
+      </Router>
     </div>
   );
 }
